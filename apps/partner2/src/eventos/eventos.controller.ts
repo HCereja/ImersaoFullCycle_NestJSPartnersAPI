402556323ac7d54646465ccd785efff3d6c78ba0
Henrique Costa Cereja <cerejahenrique@gmail.com>
2024-06-18 11:49:27 -0300

    Criação do sistema de autenticação

--- apps/partner2/src/eventos/eventos.controller.ts
+++ apps/partner2/src/eventos/eventos.controller.ts
@@ -7,11 +7,13 @@ import {
   Param,
   Delete,
   HttpCode,
+  UseGuards,
 } from '@nestjs/common';
 import { EventsService } from '@app/core/events/events.service';
 import { CriarEventoRequest } from './request/criar-evento.request';
 import { AtualizarEventoRequest } from './request/atualizar-evento.request';
 import { ReservarLugarRequest } from './request/reservar-lugar.request';
+import { AuthGuard } from '@app/core/auth/auth.guard';
 
 @Controller('eventos')
 export class EventosController {
@@ -46,6 +48,7 @@ export class EventosController {
     return this.eventosService.remove(id);
   }
 
+  @UseGuards(AuthGuard)
   @Post(':id/reserva')
   reserveSpots(
     @Param('id') eventId: string,
